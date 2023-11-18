8a990b615684274370712fbf963d76235c983e14
Abu Saebu <a.saebu@kw1c.nl>
2023-11-17 23:52:04 -0500

    gridview voor de afbeeldingen van de smoelen

--- src/views/Smoelenboek.vue
+++ src/views/Smoelenboek.vue
@@ -17,7 +17,16 @@
       </ion-header>
 
       <div id="container">
-        <h2>Alle vrijwilligers die in de VF werken</h2>
+        <ion-grid :fixed="true">
+          <!-- 3 items per row -->
+          <ion-row>
+            <ion-col v-for="item in items" :key="item.title" size="3">
+              <ion-label>
+                <h3>{{ item.title }}</h3>
+              </ion-label>
+            </ion-col>
+          </ion-row>
+      </ion-grid>
       </div>
     </ion-content>
   </ion-page>
@@ -25,26 +34,40 @@
 
 <script setup lang="ts">
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonCol, IonGrid, IonRow } from '@ionic/vue';
+
+//variable for grid items
+const items = [
+  { title: 'Item 1', note: 'This is item #1' },
+  { title: 'Item 2', note: 'This is item #2' },
+  { title: 'Item 3', note: 'This is item #3' },
+  { title: 'Item 4', note: 'This is item #4' },
+  { title: 'Item 5', note: 'This is item #5' },
+  { title: 'Item 6', note: 'This is item #6' },
+  { title: 'Item 7', note: 'This is item #7' },
+  { title: 'Item 8', note: 'This is item #8' },
+  { title: 'Item 9', note: 'This is item #9' },
+];
+
 </script>
 
 <style scoped>
-#container {
-  display: flex;
-  flex-direction: column;
-}
+ion-grid {
+    --ion-grid-width: 80%;
 
+    --ion-grid-width-xs: 50%;
+    --ion-grid-width-sm: 288px;
+    --ion-grid-width-md: 384px;
+    --ion-grid-width-lg: 480px;
+    --ion-grid-width-xl: 570px;
+    margin-inline: 2px;
+  }
 
-#container h2 {
-  font-size: 1.2rem;
-  line-height: 1.3em;
-  padding: 0.4em 0 0.5em 0.4em;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
+  ion-col {
+    background-color: #5d5d13;
+    border: solid 1px #fff;
+    color: #fff;
+    text-align: center;
+  }
 
 </style>
